b8b285da701c13d942f89076fd440c8405a2cf77
Rana Almalki <ranaalmalki12@gmail.com>
2024-09-27 15:39:46 +0300

    add the navbar components

--- my-Hw-project/src/App.jsx
+++ my-Hw-project/src/App.jsx
@@ -1,3 +1,4 @@
+import NavbarHeader from './components/NavbarHeader'
 import Footer from './components/Footer'
 import './App.css'
 
@@ -5,6 +6,7 @@ function App() {
 
   return (
     <>
+    <NavbarHeader/>
       <Footer/>
     </>
   )
