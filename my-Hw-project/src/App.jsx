98e041ac113902ab3a8bcea5a78c81edddde1932
Rana Almalki <ranaalmalki12@gmail.com>
2024-09-29 14:17:31 +0300

    add components contact

--- my-Hw-project/src/App.jsx
+++ my-Hw-project/src/App.jsx
@@ -7,19 +7,21 @@ import LatestVideos from './components/LatestVideos'
 import Join from './components/Join'
 import Footer from './components/Footer'
 import './App.css'
+import Contact from './components/Contact'
 
 function App() {
 
   return (
     <>
-      <NavbarHeader />
+      {/* <NavbarHeader />
       <Welcome />
       <Fetured/>
       <LatestReviews />
       <ClassRestaurant />
       <LatestVideos />
       <Join />
-      <Footer />
+      <Footer /> */}
+      <Contact/>
     </>
   )
 }
